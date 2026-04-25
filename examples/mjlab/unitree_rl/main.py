6fec0605c70a47b4938096fcaaa1174ecb5cd8ae
ttktjmt <tatsuki.tsujimoto@gmail.com>
2026-04-25 15:53:52 +0900

    Add skybox in the dance demo

--- examples/mjlab/unitree_rl/main.py
+++ examples/mjlab/unitree_rl/main.py
@@ -5,6 +5,7 @@ import pathlib
 import sys
 
 import mjlab.tasks  # noqa: F401 - populates the mjlab task registry
+import mujoco
 import src.tasks  # noqa: F401
 from mjlab.tasks.registry import load_env_cfg
 
@@ -17,7 +18,7 @@ importlib.import_module("terminations")  # noqa: F401 - registers custom termina
 def setup_builder() -> mjswan.Builder:
     """Create the builder for the unitree_rl_mjlab demo."""
 
-    run_path = "ttktjmt-org/mjlab/l3tgm74z"
+    run_paths = ["ttktjmt-org/mjlab/l3tgm74z", "ttktjmt-org/mjlab/7m1ycqsn"]
     task_id = "Unitree-G1-Tracking-No-State-Estimation"
 
     builder = mjswan.Builder()
@@ -25,9 +26,22 @@ def setup_builder() -> mjswan.Builder:
     project = builder.add_project(name="Unitree RL")
     scene = project.add_mjlab_scene(task_id, play=True)
 
+    # Customize skybox
+    mjspec = scene._config.spec
+    assert mjspec is not None
+    mjspec.add_texture(
+        name="skybox",
+        type=mujoco.mjtTexture.mjTEXTURE_SKYBOX,
+        builtin=mujoco.mjtBuiltin.mjBUILTIN_GRADIENT,
+        rgb1=[0.6, 0.8, 0.9],
+        rgb2=[0.9, 0.9, 0.9],
+        width=512,
+        height=512,
+    )
+
     env_cfg = load_env_cfg(task_id, play=True)
     scene.add_policy_from_wandb(
-        run_path,
+        run_paths,
         task_id=task_id,
         observations={"policy": env_cfg.observations["actor"]},
         commands=env_cfg.commands,
